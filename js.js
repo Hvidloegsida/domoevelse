b532e40e5b4b435228c338616b7fd8e9a9990acf
Hvidloegsida <idmi0001@stud.kea.dk>
2023-08-24 10:31:10 +0200

    ahhhhhhhhhhh

---
+++ js.js
@@ -0,0 +1,18 @@
+const articleh = document.querySelector("h3");
+articleh.textContent = "AHHHHHHHHHHHH";
+console.log(articleh);
+const h2 = document.querySelector("h2");
+h2.textContent = "TISSEMYRER";
+console.log(h2);
+
+const titel = document.querySelector("h1");
+titel.textContent = "HJÆLP MIG";
+console.log(titel);
+
+const pp = document.querySelector("p");
+pp.textContent = "JEG KAN IKKE MERE";
+console.log(pp);
+
+const no = document.querySelector("#no");
+no.textContent = "JEG VIL HJEM";
+console.log(no);
