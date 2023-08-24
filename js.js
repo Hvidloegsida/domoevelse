b2451ea6ad8b8a6af91f657968727ce579425769
Hvidloegsida <idmi0001@stud.kea.dk>
2023-08-24 11:09:31 +0200

    noter

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
+/*HER ÆNDRER JEG I MIN DOM (HTML)*/
+/*HER TAGER JEG FAT IO MIN H3 FRA HTML*/
 const articleh = document.querySelector("h3");
+/*HER FORTÆLLER JEG DEN V IA TEXTCONTANT HVAD DEN SKAL ERSTATTE MIN H3 MED*/
 articleh.textContent = "AHHHHHHHHHHHH";
 console.log(articleh);
+/*DONE*/
 const h2 = document.querySelector("h2");
 h2.textContent = "TISSEMYRER";
 console.log(h2);
@@ -22,8 +26,14 @@ pic.setAttribute("src", "billede.jpg");
 pic.setAttribute("alt", "computer");
 console.log(pic);
 
+/* HER LAVER VI ET HELT NYT ELEMEMENT.*/
+/*TAGER FAT I BODY (FRA HTML)*/
 const bodyy = document.querySelector("body");
+/*LAVER ELEMENT MED CREATEELEMENT, SOM JEG VÆLGER SKAL VÆRE EN H1*/
 const overskrift = document.createElement("h1");
+/*HER LAVER JEG ET ELEMENT MED CREATETEXTNODE, SOM JEG VÆLGER SKAL SKRIVE NOOO*/
 const text = document.createTextNode("NOOOOOOO");
+/*HER KOMIBERER JEG MIN OVERSKRIFT CONSTANT MED MIN TEXT CONSTANT, SÅ AT TEKSTEN KOMMER IND I OVERSKRIFTEN*/
 overskrift.appendChild(text);
+/* HER GØR JEG SÅ BRUG AF MIT BODY TAG IGEN, VED AT SMIDE DEN OVERSKRIFT JEG LAVEDE FØR VED BRUG AF MIN TEXT. SÅ VED JS AT MIN H1 SKAL VÆRE INDE I BODY*/
 bodyy.appendChild(overskrift);
